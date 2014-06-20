d89046a7993c6a90ffec4751698831410858fb9c
liuliming <liuliming@xinnet.com>
2014-06-20 10:28:33 +0800

    add css/lexin_index/css_custom.php

---
+++ mod/lexin_index/views/default/css/lexin_index/css_custom.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Custom Index CSS1
+ *
+ */
+?>
+
+/*******************************
+	Custom Index
+********************************/
+.custom-index {
+	padding: 10px 0;
+}
+.custom-index-col1 > .elgg-inner {
+	padding-right: 10px;
+}
+.custom-index-col2 > .elgg-inner {
+	padding-left: 10px;
+}
+.elgg-module-highlight {
+	box-shadow: 1px 1px 5px #CCC;
+}
+.elgg-module-highlight:hover {
+	box-shadow: 1px 1px 6px #AAA;
+}
+
+.elgg-page-body{
+  min-height:632px;
+}
+.elgg-col-1of2 {
+ width:50%;  
+}
+.elgg-col-2of2 {
+ width:45%;    
+ float:right;
+}
+
+
+ 
+.custom-index .elgg-module {
+   background: none repeat scroll 0 0 #FFFFFF;
+}
