0b3f3e127695deebc176397c3c24c916591c40bd
liuliming <liuliming@xinnet.com>
2014-08-01 15:15:38 +0800

    fix editor

--- mod/ckeditor/views/default/js/elgg/ckeditor/config.js
+++ mod/ckeditor/views/default/js/elgg/ckeditor/config.js
@@ -3,9 +3,26 @@ define(function(require) {
 	var $ = require('jquery');
 
 	return {
-		toolbar: [['Bold', 'Italic', 'Underline'], ['Strike', 'NumberedList', 'BulletedList', 'Undo', 'Redo', 'Link', 'Unlink', 'Image', 'Blockquote', 'Paste', 'PasteFromWord', 'Maximize']],
-		removeButtons: 'Subscript,Superscript', // To have Underline back
-		allowedContent: true,
+		//toolbar: [['Bold', 'Italic', 'Underline'], ['Strike', 'NumberedList', 'BulletedList', 'Undo', 'Redo', 'Link', 'Unlink', 'Image', 'Blockquote', 'Paste', 'PasteFromWord', 'Maximize']],
+		//removeButtons: 'Subscript,Superscript', // To have Underline back
+    //removeDialogTabs: 'image:advanced;image:Link;link:advanced;link:target',
+    plugins: 'about,basicstyles,clipboard,floatingspace,list,indentlist,enterkey,entities,link,toolbar,undo,wysiwygarea',
+    removeButtons: 'Anchor,Underline,Strike,Subscript,Superscript',
+    toolbarGroups: [
+      { name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+      { name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
+      { name: 'forms' },
+      { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+      { name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align' ] },
+      { name: 'links' },
+      { name: 'insert' },
+      { name: 'styles' },
+      { name: 'colors' },
+      { name: 'tools' },
+      { name: 'others' },
+      { name: 'about' }
+    ],
+    allowedContent: true,
 		baseHref: elgg.config.wwwroot,
 		removePlugins: 'contextmenu,tabletools,resize',
 		defaultLanguage: 'en',
@@ -15,7 +32,7 @@ define(function(require) {
 		contentsCss: elgg.get_simplecache_url('css', 'elgg/wysiwyg.css'),
 		disableNativeSpellChecker: false,
 		disableNativeTableHandles: false,
-		removeDialogTabs: 'image:advanced;image:Link;link:advanced;link:target',
+		
 		autoGrow_maxHeight: $(window).height() - 100
 	};
 });
