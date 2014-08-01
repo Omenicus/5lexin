76ab910e36afa0a21f90733a279d7285ab85711f
liuliming <liuliming@xinnet.com>
2014-08-01 15:51:18 +0800

    fix editor

--- mod/ckeditor/views/default/js/elgg/ckeditor/config.js
+++ mod/ckeditor/views/default/js/elgg/ckeditor/config.js
@@ -3,10 +3,10 @@ define(function(require) {
 	var $ = require('jquery');
 
 	return {
-		//toolbar: [['Bold', 'Italic', 'Underline'], ['Strike', 'NumberedList', 'BulletedList', 'Undo', 'Redo', 'Link', 'Unlink', 'Image', 'Blockquote', 'Paste', 'PasteFromWord', 'Maximize']],
-		//removeButtons: 'Subscript,Superscript', // To have Underline back
-    //removeDialogTabs: 'image:advanced;image:Link;link:advanced;link:target',
-    plugins: 'about,basicstyles,clipboard,floatingspace,list,indentlist,enterkey,entities,link,toolbar,undo,wysiwygarea',
+		toolbar: [['Bold', 'Italic', 'Underline'], ['Strike', 'NumberedList', 'BulletedList', 'Undo', 'Redo', 'Link', 'Unlink', 'Image','Table', 'Blockquote', 'Paste', 'PasteFromWord', 'Maximize']],
+		removeButtons: 'Subscript,Superscript', // To have Underline back
+    removeDialogTabs: 'image:advanced;image:Link;link:advanced;link:target',
+    /*plugins: 'about,basicstyles,clipboard,floatingspace,list,indentlist,enterkey,entities,link,toolbar,undo,wysiwygarea',
     removeButtons: 'Anchor,Underline,Strike,Subscript,Superscript',
     toolbarGroups: [
       { name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
@@ -21,12 +21,13 @@ define(function(require) {
       { name: 'tools' },
       { name: 'others' },
       { name: 'about' }
-    ],
+    ],*/
     allowedContent: true,
 		baseHref: elgg.config.wwwroot,
-		removePlugins: 'contextmenu,tabletools,resize',
-		defaultLanguage: 'en',
-		language: elgg.config.language,
+		//removePlugins: 'contextmenu,tabletools,resize',
+		defaultLanguage: 'zh-cn',
+    language = 'zh-cn';
+		//language: elgg.config.language,
 		skin: 'moono',
 		uiColor: '#EEEEEE',
 		contentsCss: elgg.get_simplecache_url('css', 'elgg/wysiwyg.css'),
