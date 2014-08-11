c4c71443d92091bb2baa9452818b4ab60c6240b0
liuliming <liuliming@xinnet.com>
2014-08-11 14:04:29 +0800

    fix

--- mod/thewire_tools/lib/events.php
+++ mod/thewire_tools/lib/events.php
@@ -23,14 +23,14 @@
             $user=get_user_by_name($username);             
 					if ($user )//&& ($user->getGUID() != $object->getOwnerGUID())) 
           {
-						//if (elgg_get_plugin_user_setting("notify_mention", $user->getGUID(), "thewire_tools") == "yes") 
+						if (elgg_get_plugin_user_setting("notify_mention", $user->getGUID(), "thewire_tools") == "yes") 
             {
 							$subject = elgg_echo("thewire_tools:notify:mention:subject");
 							$message = elgg_echo("thewire_tools:notify:mention:message", array(
 												$user->name,
 												$object->getOwnerEntity()->name,
 												elgg_get_site_url() . "thewire/search/@" . $user->username));
-							system_message( $subject.$message);
+							//debug system_message( $subject.$message);
 							notify_user($user->getGUID(), $object->getOwnerGUID(), $subject, $message);
 						}
 					}
